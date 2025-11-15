9cbc0302095fc6c782a40d932bb3f1364ece87fe
Yacine <yacine.madani.pro@gmail.com>
2025-11-15 11:45:52 +0100

    file(Lexical_analyzer): added unitType method --NOT COMPLETE--

--- source_code/src/main/java/com/mini_compiler/Lexical_analyzer.java
+++ source_code/src/main/java/com/mini_compiler/Lexical_analyzer.java
@@ -1,5 +1,12 @@
 package com.mini_compiler;
 
+/**
+ * @author Yacine Madani
+ * @version 1.0
+ * @since November 2025
+ */
+
+
 import java.util.Scanner;
 import java.io.File;
 import java.io.FileNotFoundException;
@@ -13,14 +20,46 @@ public class Lexical_analyzer {
     
     private static final String[] SEPARATORS = {"(",")","{","}","[","]",",",";",":",".","'","\""};
 
+    private static final int LEXICAL_ERROR = 2;
+    
+
+    public static String unitType(String lexical_unit){
+        
+        for (String keyword : KEYWORDS) {
+            if (lexical_unit == keyword) {
+                return "KEYWORD";
+            }
+        }
+        for (String special : SPECIAL_KEYWORD) {
+            if (lexical_unit == special) {
+                return "SPECIAL_KEYWORD";
+            }
+        }
+        for (String operator : OPERATORS) {
+            if (lexical_unit == operator) {
+                return "OPERATOR";
+            }
+        }
+        for (String separator : SEPARATORS) {
+            if (lexical_unit == separator) {
+                return "SEPARATOR";
+            }
+        }
+        
+
+        //CALL ERROR FUNCTION HERE !!!!
+
+        return "error";
+    }
+    
     //--- Method to analyze the given code ---
 
-    
     public static void Analyzer() throws FileNotFoundException{
         String lexical_unit = "";
         String input = "";
         int i = 0;
 
+            //Reading input from a file "code.dat"
             try {
             Scanner scan = new Scanner(new File("Mini-Compilateur-Python/executable/code.dat"));
             
@@ -29,7 +68,7 @@ public class Lexical_analyzer {
                 i = 0;
 
                 while(input.charAt(i) != '#'){
-                    
+
                 }
                 
 
